7332790b33e68dc33164ab67d3c40838a770baf2
weiwei.huang <weiwei.huang@tcl.com>
2016-01-14 13:41:34 +0800

    zhushi

--- spider_main.py
+++ spider_main.py
@@ -32,5 +32,5 @@ class SpiderMain(object):
 if __name__ == "__main__":
 	root_url = "http://baike.baidu.com/view/21087.htm"
 	obj_spider = SpiderMain()
-	obj_spider.craw(root_url)
+	obj_spider.craw(root_url)#weiwei.huang
 
